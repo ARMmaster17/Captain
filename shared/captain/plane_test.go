ae4394ac700c14d9cb11fd6ae5978b83d026533f
Joshua Zenn <wordman05@gmail.com>
2021-03-18 09:01:02 -0400

    #20 WIP Unit testing for plane verification

---
+++ shared/captain/plane_test.go
@@ -0,0 +1,36 @@
+package captain
+
+import (
+	"testing"
+)
+
+func Test_NewPlane(t *testing.T) {
+
+}
+
+func plane_helper(t *testing.T, name string, cpu int, ram int, storage int, shouldError bool) {
+	plane, err := NewPlane(name, cpu, ram, storage)
+	if err != nil && !shouldError {
+		t.Logf("expected err to be nil with input {%s,%d,%d,%d}, got %s", name, cpu, ram, storage, err)
+		t.Fail()
+	}
+	if err != nil {
+		return
+	}
+	if plane.Name != name {
+		t.Logf("expected Name to be %s with input {%s,%d,%d,%d}, got %s", name, name, cpu, ram, storage, plane.Name)
+		t.Fail()
+	}
+	if plane.CPU != cpu {
+		t.Logf("expected CPU to be %d with input {%s,%d,%d,%d}, got %d", cpu, name, cpu, ram, storage, plane.CPU)
+		t.Fail()
+	}
+	if plane.RAM != ram {
+		t.Logf("expected RAM to be %d with input {%s,%d,%d,%d}, got %d", ram, name, cpu, ram, storage, plane.RAM)
+		t.Fail()
+	}
+	if plane.Storage != storage {
+		t.Logf("expected CPU to be %d with input {%s,%d,%d,%d}, got %d", cpu, name, cpu, ram, storage, plane.CPU)
+		t.Fail()
+	}
+}
