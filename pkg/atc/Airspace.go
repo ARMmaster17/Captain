41d1a6ea5cb1564513d1f6a74b8d499a6d6d7175
Joshua Zenn <wordman05@gmail.com>
2021-04-03 23:05:33 -0400

    More DB backend stuff

--- pkg/atc/Airspace.go
+++ pkg/atc/Airspace.go
@@ -19,23 +19,40 @@ func CreateAirspace(humanName string, netName string) (*Airspace, error) {
 }
 
 func FindByID(id int) (*Airspace, error) {
-	results, err := DBQueryWithParams("SELECT * FROM airspace WHERE id = ?", strconv.Itoa(id))
+	results, err := DBQueryWithParams("SELECT * FROM airspace WHERE AirspaceID = ?", strconv.Itoa(id))
 	if err != nil {
 		return &Airspace{}, fmt.Errorf("unable to find airspace ID %d with error: %w", id, err)
 	}
 	// Scan results and return first value
+	if !results.Next() {
+		return &Airspace{}, fmt.Errorf("no airspace found with ID %d", id)
+	}
+	var idx int
+	var humanName string
+	var netName string
+	err = results.Scan(&idx, &humanName, &netName)
+	if err != nil {
+		return &Airspace{}, fmt.Errorf("unable to find airspace ID %d with error: %w", id, err)
+	}
+	return &Airspace{
+		AirspaceID: idx,
+		HumanName: humanName,
+		NetName: netName,
+	}, nil
 }
 
 func (a *Airspace)Save() error {
-	database, err := getDBConnection()
+	err := DBExecuteWithParams("UPDATE airspace SET HumanName = ?, NetName = ? WHERE AirspaceID = ?", a.HumanName, a.NetName, strconv.Itoa(a.AirspaceID))
 	if err != nil {
 		return fmt.Errorf("unable to save airspace '%s' with error: %w", a.HumanName, err)
 	}
+	return nil
 }
 
 func (a *Airspace)Delete() error {
-	database, err := getDBConnection()
+	err := DBExecuteWithParams("DELETE FROM airspace WHERE AirspaceID = ?", strconv.Itoa(a.AirspaceID))
 	if err != nil {
 		return fmt.Errorf("unable to delete airspace '%s' with error: %w", a.HumanName, err)
 	}
+	return nil
 }
