8ba70f7e2edececacb8c87458e94df5cd7271fba
Joshua Zenn <wordman05@gmail.com>
2021-04-09 23:09:59 -0400

    Fixed duplicate main()

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"flag"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
 )
 
 func main() {
+	bootstrapOnly := flag.Bool("boostrap", false, "runs stripped-down version of Captain to build the Captain stack from a single worker node.")
+
+
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	// TODO: Read verbosity level from command line args
 
 	log.Info().Msg("Captain v0.0.0 is starting up")
+
+	if *bootstrapOnly {
+		err := BootstrapCluster()
+		if err != nil {
+			log.Error().Msg("An error occurred: " + err.Error())
+		}
+		return
+	}
 
 	err := StartMonitoring()
 	if err != nil {
