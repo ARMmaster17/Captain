4e949f35678d3946eb167e87c4bd7281de68b2db
Joshua Zenn <wordman05@gmail.com>
2021-05-22 23:07:34 -0400

    #63 Config generation fixes for test environment

--- ATC/main_test.go
+++ ATC/main_test.go
@@ -9,12 +9,17 @@ import (
 
 func TestGenerateConfigFile(t *testing.T) {
 	_ = os.Remove("/etc/captain/atc/config.yaml")
+	_, err := os.Create("/etc/captain/atc/config.yaml")
+	assert.NoError(t, err)
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("/etc/captain/atc")
-	err := generateConfigFile()
+	err = generateConfigFile()
 	assert.NoError(t, err)
 	info, err := os.Stat("/etc/captain/atc/config.yaml")
 	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
 	assert.Greater(t, info.Size(), int64(0))
 }
