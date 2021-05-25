22b1e9d1e642b421baf065c18e16b4eeba900269
Joshua Zenn <wordman05@gmail.com>
2021-05-24 21:58:52 -0400

    #56 WIP IPAM address incrementation

--- ATC/IPAM/ReservedBlock.go
+++ ATC/IPAM/ReservedBlock.go
@@ -1,7 +1,9 @@
 package IPAM
 
 import (
+	"fmt"
 	"gorm.io/gorm"
+	"math"
 	"net"
 )
 
@@ -10,4 +12,53 @@ type ReservedBlock struct {
 	BlockName string
 	Subnet    net.IPNet
 	Addresses []ReservedAddress
+}
+
+func (ipam *IPAM) getAllReservedBlocks() ([]ReservedBlock, error) {
+	var allBlocks []ReservedBlock
+	result := ipam.db.Find(&allBlocks)
+	if result.Error != nil {
+		return nil, fmt.Errorf("unable to get IP blocks from database: %w", result.Error)
+	}
+	return allBlocks, nil
+}
+
+func (r *ReservedBlock) hasAvailableAddress(db *gorm.DB) (bool, error) {
+	var addressCountInBlock int64
+	result := db.Model(&ReservedAddress{}).Where("reservedblock_id = ?", r.ID).Count(&addressCountInBlock)
+	if result.Error != nil {
+		return false, fmt.Errorf("unable to get list of addresses in block: %w", result.Error)
+	}
+	return !subnetIsFull(addressCountInBlock, r.Subnet.Mask), nil
+}
+
+func (r *ReservedBlock) reserveAddress(db *gorm.DB) (net.IP, error) {
+	ip, err := r.getNextAddress(db)
+}
+
+func (r *ReservedBlock) getNextAddress(db *gorm.DB) (net.IP, error) {
+	for i := 0; int64(i) < getSubnetAddressSize(r.Subnet.Mask); i++ {
+		ip := nextIP(r.Subnet.IP, uint(i))
+		inUse, err := r.addressIsInUse(ip)
+		if err != nil {
+			return net.IP{}, fmt.Errorf("unable to check if next address is in use: %w", err)
+		} else if !inUse {
+			return ip, nil
+		}
+	}
+	// This shouldn't happen because we already validate that the block has at least one open address.
+	return net.IP{}, fmt.Errorf("the IP block metadata and address table have become desyncronized")
+}
+
+func (r *ReservedBlock) addressIsInUse(ip net.IP) (bool, error) {
+
+}
+
+func subnetIsFull(existingAddresses int64, subnetCIDR net.IPMask) bool {
+	return existingAddresses >= getSubnetAddressSize(subnetCIDR)
+}
+
+func getSubnetAddressSize(subnetCIDR net.IPMask) int64 {
+	leadingOnes, bits := subnetCIDR.Size()
+	return int64(math.Pow(2, float64(bits - leadingOnes)) - 2)
 }
--- ATC/IPAM/ipam.go
+++ ATC/IPAM/ipam.go
@@ -24,28 +24,80 @@ func (ipam *IPAM) Initialize(db *gorm.DB) error {
 		return fmt.Errorf("unable to perform IPAM database migrations: %w", err)
 	}
 	ipam.mutex = &sync.Mutex{}
-	ipam.syncDBBlocksWithConfig(viper.GetStringSlice("defaults.network.blocks"))
+	err = ipam.syncDBBlocksWithConfig(viper.GetStringSlice("defaults.network.blocks"))
+	if err != nil {
+		return fmt.Errorf("unable to sync database with new IP block config: %w", err)
+	}
 	return nil
 }
 
-func (ipam *IPAM) GetNextAddress() (net.IPAddr, error) {
+func (ipam *IPAM) GetNewAddress() (net.IPAddr, error) {
 	ipam.mutex.Lock()
-
+	freeBlock, err := ipam.findFreeBlock()
+	if err != nil {
+		ipam.mutex.Unlock()
+		return net.IPAddr{}, fmt.Errorf("unable to find a free IP block: %w", err)
+	}
+	newAddress, err := freeBlock.reserveAddress(ipam.db)
 	ipam.mutex.Unlock()
+	return newAddress, err
 }
 
 func (ipam *IPAM) ReleaseAddress(addr net.IPAddr) error {
 	ipam.mutex.Lock()
-
+	// TODO: Not implemented.
 	ipam.mutex.Unlock()
+	return nil
 }
 
 func (ipam *IPAM) syncDBBlocksWithConfig(blocks []string) error {
 	netBlocks := parseSubnetBlocks(blocks)
-	var existingReservedBlocks []ReservedBlock
-	result := ipam.db.Find(&existingReservedBlocks)
-	if result.Error != nil {
-		return fmt.Errorf("unable to retrieve list of reserved IP blocks: %w", result.Error)
+	existingReservedBlocks, err := ipam.getAllReservedBlocks()
+	if err != nil {
+		return fmt.Errorf("unable to retrieve list of reserved IP blocks: %w", err)
 	}
+	for _, netBlock := range netBlocks {
+		var err error
+		existingReservedBlocks, err = ipam.addNetblockIfNotExists(netBlock, existingReservedBlocks)
+		if err != nil {
+			return fmt.Errorf("unable to merge IP block configuration with database: %w", err)
+		}
+	}
+	// TODO: Remove blocks that are no longer in the config. This will require deleting planes, and not allowing
+	// new planes to be built until the IP block table is fully synced.
+	return nil
+}
 
+func (ipam *IPAM) addNetblockIfNotExists(netBlock net.IPNet, existingReservedBlocks []ReservedBlock) ([]ReservedBlock, error) {
+	for _, block := range existingReservedBlocks {
+		if block.BlockName == netBlock.String() {
+			return existingReservedBlocks, nil
+		}
+	}
+	// Block does not exist, create it.
+	newBlock := ReservedBlock{
+		BlockName: netBlock.String(),
+		Subnet:    netBlock,
+	}
+	result := ipam.db.Create(&newBlock)
+	if result.Error != nil {
+		return existingReservedBlocks, fmt.Errorf("unable to create new reserved IP block: %w", result.Error)
+	}
+	return append(existingReservedBlocks, newBlock), nil
+}
+
+func (ipam *IPAM) findFreeBlock() (ReservedBlock, error) {
+	reservedBlocks, err := ipam.getAllReservedBlocks()
+	if err != nil {
+		return ReservedBlock{}, fmt.Errorf("unable to get IP block list: %w", err)
+	}
+	for _, block := range reservedBlocks {
+		isAvailable, err := block.hasAvailableAddress(ipam.db)
+		if err != nil {
+			return ReservedBlock{}, fmt.Errorf("unable to check if block has free addresses: %w", err)
+		} else if isAvailable {
+			return block, nil
+		}
+	}
+	return ReservedBlock{}, fmt.Errorf("all reserved IP blocks are full")
 }
