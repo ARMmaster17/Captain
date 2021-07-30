bb83cf2d161c190cb77b3f8ed426f3eaad95d18e
Joshua Zenn <wordman05@gmail.com>
2021-07-29 22:02:48 -0400

    #138 Plane CRUD model implementation

--- Shared/models/plane_test.go
+++ Shared/models/plane_test.go
@@ -1,7 +1,13 @@
 package models
 
 import (
+	"github.com/ARMmaster17/Captain/Shared/framework"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"regexp"
 	"testing"
+	"time"
 )
 
 func TestValidateName(t *testing.T) {
@@ -218,4 +224,74 @@ func TestValidateDisk(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_PlaneCreate(t *testing.T) {
+	f, err := framework.NewFramework("test")
+	require.NoError(t, err)
+	mockDB := HelperInitMockDB(t, f)
+	plane := Plane{}
+	plane.CreatedAt = time.Now()
+	plane.UpdatedAt = time.Now()
+	mockDB.ExpectBegin()
+	mockDB.ExpectExec(regexp.QuoteMeta("INSERT INTO `planes` (`created_at`,`updated_at`,`deleted_at`,`name`,`cpu`,`ram`,`disk`) VALUES (?,?,?,?,?,?,?)")).
+		WithArgs(plane.CreatedAt, plane.UpdatedAt, plane.DeletedAt, plane.Name, plane.CPU, plane.RAM, plane.Disk).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mockDB.ExpectCommit()
+	mockDB.ExpectRollback()
+	err = plane.Create(f.DB)
+	assert.NoError(t, err)
+}
+
+func Test_PlaneGetByID(t *testing.T) {
+	f, err := framework.NewFramework("test")
+	require.NoError(t, err)
+	mockDB := HelperInitMockDB(t, f)
+	mockDB.NewRows([]string{"id"}).AddRow("0")
+	plane := Plane{}
+	plane.CreatedAt = time.Now()
+	plane.UpdatedAt = time.Now()
+	mockDB.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `planes` WHERE `planes`.`id` = ? AND `planes`.`deleted_at` IS NULL ORDER BY `planes`.`id` LIMIT 1")).
+		WithArgs(0).WillReturnRows(mockDB.NewRows([]string{"id"}).AddRow("0"))
+	mockDB.ExpectCommit()
+	mockDB.ExpectRollback()
+	err = plane.GetByID(f.DB, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, uint(0), plane.ID)
+}
+
+func Test_PlaneUpdate(t *testing.T) {
+	f, err := framework.NewFramework("test")
+	require.NoError(t, err)
+	mockDB := HelperInitMockDB(t, f)
+	plane := Plane{}
+	plane.CreatedAt = time.Now()
+	plane.UpdatedAt = time.Now()
+	plane.ID = 1
+	mockDB.ExpectBegin()
+	mockDB.ExpectExec(regexp.QuoteMeta("UPDATE `planes` SET `created_at`=?,`updated_at`=?,`deleted_at`=?,`name`=?,`cpu`=?,`ram`=?,`disk`=? WHERE `id` = ?")).
+		WithArgs(plane.CreatedAt, sqlmock.AnyArg(), plane.DeletedAt, plane.Name, plane.CPU, plane.RAM, plane.Disk, plane.ID).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mockDB.ExpectCommit()
+	mockDB.ExpectRollback()
+	err = plane.Update(f.DB)
+	require.NoError(t, err)
+}
+
+func Test_PlaneDelete(t *testing.T) {
+	f, err := framework.NewFramework("test")
+	require.NoError(t, err)
+	mockDB := HelperInitMockDB(t, f)
+	plane := Plane{}
+	plane.CreatedAt = time.Now()
+	plane.UpdatedAt = time.Now()
+	plane.ID = 1
+	mockDB.ExpectBegin()
+	mockDB.ExpectExec(regexp.QuoteMeta("UPDATE `planes` SET `deleted_at`=? WHERE `planes`.`id` = ? AND `planes`.`deleted_at` IS NULL")).
+		WithArgs(sqlmock.AnyArg(), plane.ID).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mockDB.ExpectCommit()
+	mockDB.ExpectRollback()
+	err = plane.Delete(f.DB)
+	require.NoError(t, err)
 }
